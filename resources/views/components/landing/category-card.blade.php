8a56f96f73a41672325be45c1466d8fbbb935bd9
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2024-07-02 20:04:51 +0330

    add category card component

---
+++ resources/views/components/landing/category-card.blade.php
@@ -0,0 +1,10 @@
+<div class="p-3">
+    <a href="#" class="flex flex-col items-center">
+        <div>
+            <img src="https://www.zagros.market/uploads/IMG-0068-500x500.jpg" class="w-44" alt="">
+        </div>
+        <p class="text-center">
+            پروتئینی
+        </p>
+    </a>
+</div>
