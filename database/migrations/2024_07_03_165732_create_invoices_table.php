5986d94e8a1232c244029b0b76e18df53be7364b
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2024-07-04 13:05:59 +0330

    add models and database

---
+++ database/migrations/2024_07_03_165732_create_invoices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Wallet;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Wallet::class)->constrained();
+            $table->integer('price');
+            $table->tinyInteger('status')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
---
+++ database/migrations/2024_07_03_165812_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Brand;
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->string('image');
+            $table->text('description');
+            $table->integer('price');
+            $table->foreignIdFor(Category::class)->constrained();
+            $table->foreignIdFor(Brand::class)->constrained();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
